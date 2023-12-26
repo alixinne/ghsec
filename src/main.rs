c89ae3cb91bf8638b1868c0e0a83c4cc3193af0f
Vincent Tavernier <v.tavernier@pm.me>
2023-12-26 23:40:56 +0100

    docs: add main module documentation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+//! # ghsec
+//!
+//! [![main](https://github.com/vtavernier/ghsec/actions/workflows/main.yml/badge.svg?event=push)](https://github.com/vtavernier/ghsec/actions/workflows/main.yml)
+//!
+//! ghsec is an opinionated linter (with fixes) for public GitHub repository security. It helps
+//! diagnose and fix potential security issues caused by GitHub repository settings that are
+//! usually too open by default.
+//!
+//! ## Installation
+//!
+//! ### From source
+//!
+//! ```bash
+//! cargo install --git https://github.com/vtavernier/ghsec.git
+//! ```
+//!
+//! ## Usage
+//!
+//! You will need a personal access token with admin access level to your repositories. Currently,
+//! this tool has only been tested with classic tokens with the repo scope.
+//!
+//! ```bash
+//! # Provide a GitHub personal access token with admin access to your repositories
+//! export GITHUB_TOKEN=ghp_.....
+//!
+//! # Run the checks
+//! ghsec
+//!
+//! # Run the checks and fix the issues, if possible
+//! ghsec --fix
+//! ```
+//!
+//! ## Supported checks
+//! 
+//! - [`default_workflow_permissions`](checks/default_worfklow_permissions/index.html): use secure
+//! defaults for "Default Workflow Permissions"
+//! - [`repository_secrets`](checks/repository_secrets/index.html): list repositories containing
+//! GitHub Actions secrets
+
 use std::str::FromStr;
 
 use clap::Parser;
@@ -10,7 +49,7 @@ use tracing_subscriber::{filter::Directive, EnvFilter};
 mod args;
 use args::Args;
 
-mod checks;
+pub mod checks;
 use checks::{Check, CheckCtx};
 
 #[tracing::instrument(name="repository", level="info", skip_all, fields(repository = repository.full_name.as_ref().unwrap()))]
